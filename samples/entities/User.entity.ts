d23c7763ca4e2e0e5fe362d153e8da479121a69b
Jeremy Cloutier <jcloutier24@yahoo.com>
2021-10-20 23:16:54 -0400

    updated dependencies

--- samples/entities/User.entity.ts
+++ samples/entities/User.entity.ts
@@ -16,9 +16,15 @@ export class User {
     @Column()
     public active: boolean;
 
-    @OneToMany((type) => Post, (post) => post.author)
+    @OneToMany((type) => Post, (post) => post.author, {
+        nullable: false,
+        cascade: true,
+    })
     public posts: Post[];
 
-    @OneToMany((type) => Comment, (comment) => comment.user)
+    @OneToMany((type) => Comment, (comment) => comment.user, {
+        nullable: false,
+        cascade: true,
+    })
     public comments: Comment[];
 }
